b334ef5489bc9c37f5a9e3ffa8b84ca2a3aec91a
devlikepro <155617407+devlikepro@users.noreply.github.com>
2024-06-12 08:35:01 +0000

    Deploying to gh-pages from @ devlikeapro/waha-docs@65f29226c8e55f6d5daacfc3c7b5810456eeb445 🚀

--- index.min.251f7fdb452baf706f3860b54a4d2ce3919b63f0c62a53daba5a7e4d06ff7776bda82dd945008d070e34bf12fb82df957d14305c90564418f15d158660f34830.js
+++ index.min.251f7fdb452baf706f3860b54a4d2ce3919b63f0c62a53daba5a7e4d06ff7776bda82dd945008d070e34bf12fb82df957d14305c90564418f15d158660f34830.js
@@ -295,7 +295,8 @@ WHATSAPP_RESTART_ALL_SESSIONS=True: Set this variable to True to start all STOPP
 `}),e.add({id:14,href:"/docs/how-to/proxy/",title:"🌐 Proxy",description:"Proxy",content:`Overview #If you\u0026rsquo;re experiencing issues scanning QR codes in WhatsApp, especially with Indian 🇮🇳 phone numbers, using a proxy located close to the phone number\u0026rsquo;s location may help resolve the problem.
 It affects both WEBJS and NOWEB engines.
 👉 If proxy doesn\u0026rsquo;t help but the project works on YOUR local laptop - check Tunneling.
-Symptoms #You start a new session in WhatsApp. Scan the QR code. Experience a long loading time. Face login failures or instant logout. Encounter a new QR code or enter a FAILED state immediately. Configuration #There are two ways to set up proxies:
+Symptoms #You start a new session in WhatsApp. Scan the QR code. Experience a long loading time. Face login failures or instant logout. Encounter a new QR code or enter a FAILED state immediately. Solutions #There are two ways to solve this issue:
+Use a proxy (read this page and article about WAHA + Geonode). Running WAHA on local network and expose WAHA with Ngrok. Configuration #There are two ways to set up proxies:
 Global Setting (for all sessions per container) Per Session Configuration (you can define a proxy for each session when you start it) Global Proxy Configuration #To use a proxy, you can set the following environment variables:
 WHATSAPP_PROXY_SERVER=localhost:3128: Set the proxy server in the format host:port, without HTTP or HTTPS. WHATSAPP_PROXY_SERVER_USERNAME=username and WHATSAPP_PROXY_SERVER_PASSWORD=password: Configure credentials for the proxy. WHATSAPP_PROXY_SERVER_LIST=host1.example.com:3138,host2.example.com:3138: Specify a comma-separated list of addresses to use, utilizing a round-robin algorithm for server selection. WHATSAPP_PROXY_SERVER_INDEX_PREFIX=proxy-: Define a session name prefix to choose the appropriate proxy from the list. Read more about it on Configuration page -\u0026gt;.
 Session Proxy Configuration #You can configure proxy for a session by setting config.proxy fields when you POST /api/sessions/start:
@@ -303,10 +304,10 @@ server - proxy server address, without http:// or https:// prefixes username and
 { \u0026quot;name\u0026quot;: \u0026quot;default\u0026quot;, \u0026quot;config\u0026quot;: { \u0026quot;proxy\u0026quot;: { \u0026quot;server\u0026quot;: \u0026quot;localhost:3128\u0026quot; } } } Proxy with authentication
 { \u0026quot;name\u0026quot;: \u0026quot;default\u0026quot;, \u0026quot;config\u0026quot;: { \u0026quot;proxy\u0026quot;: { \u0026quot;server\u0026quot;: \u0026quot;localhost:3128\u0026quot;, \u0026quot;username\u0026quot;: \u0026quot;username\u0026quot;, \u0026quot;password\u0026quot;: \u0026quot;P@ssw0rd\u0026quot; } } } The configuration is saved and will be applied if the docker container restarts, and you set WHATSAPP_RESTART_ALL_SESSIONS environment variables. Read more about it in Autostart section.
 Read more about it on Sessions page -\u0026gt;.
-Recommended Proxies #Proxy6 #We recommend using Proxy6 where you can get a DEDICATED proxy (only you\u0026rsquo;ll use it) for \$1.77 per month.
-Apply the promocode get a 5% discount on your purchase.
-9W9oVxx3UX Geonode #Another option is Geonode.
-Tunneling #If you can\u0026rsquo;t use a proxy (or it doesn\u0026rsquo;t help), you can try tunneling the connection to your local machine.
+Recommended Proxies #Geonode #We recommend using Geonode.
+Proxy6 #Another option is Proxy6 where you can get a dedicated proxy (only you\u0026rsquo;ll use it) for \$1.77 per month.
+👉 Unfortunately, it uses VPS IP ranges, so doesn't work 100% Apply the promocode get a 5% discount on your purchase.
+9W9oVxx3UX Tunneling #If you can\u0026rsquo;t use a proxy (or it doesn\u0026rsquo;t help), you can try tunneling the connection to your local machine.
 You can run WAHA on local network then, you can buy something like RaspberryPi or a small server (lenovo m93 or similar) and host it there.
 So the setup would look like:
 You have an app (with your logic and database) running on VPS You have WAHA running on your local network on some dedicated PC (mongodb or file MUST be placed on this server as well) You have ngrok (or any similar reverse proxy) running on local network and sharing 3000 port on dedicated name (paid ngrok required for that) Your App communicated with WAHA API using the external ngrok name and WAHA send webhooks back to the server There\u0026rsquo;s nothing we can do about network detection from Meta side :(
