8f91862b4e8e9423743b4bf20aefe133f0133f04
devlikepro <waha@devlike.pro>
2024-06-24 19:39:19 +0700

    dashboard

--- static/dashboard/_nuxt/BoCTx7Z9.js
+++
@@ -1,1 +0,0 @@
-import{ax as r}from"./BvpvzN7L.js";var e=r();export{e as O};
--- static/dashboard/_nuxt/CDod0-qO.js
+++
@@ -1,1 +0,0 @@
-import r from"./DMpdxz9L.js";import{P as t,t as o}from"./BvpvzN7L.js";import"./Btrd6Tw3.js";import"./Csu0xQ_P.js";const i={__name:"AppSidebar",setup(e){return(p,a)=>(o(),t(r))}};export{i as default};
