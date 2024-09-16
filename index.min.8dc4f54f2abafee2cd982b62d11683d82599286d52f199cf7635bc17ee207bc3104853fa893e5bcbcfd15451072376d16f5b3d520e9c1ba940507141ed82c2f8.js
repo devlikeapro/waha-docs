6b52f0ffe424c3d187171bf53cd8b120226b0958
devlikepro <155617407+devlikepro@users.noreply.github.com>
2024-09-16 04:43:11 +0000

    Deploying to gh-pages from @ devlikeapro/waha-docs@2b73566e24eea5ee551b9662c91c72b248df8f47 🚀

--- index.min.8dc4f54f2abafee2cd982b62d11683d82599286d52f199cf7635bc17ee207bc3104853fa893e5bcbcfd15451072376d16f5b3d520e9c1ba940507141ed82c2f8.js
+++ index.min.8dc4f54f2abafee2cd982b62d11683d82599286d52f199cf7635bc17ee207bc3104853fa893e5bcbcfd15451072376d16f5b3d520e9c1ba940507141ed82c2f8.js
@@ -771,7 +771,7 @@ Click to open full size. Click to open full size. Workflow Examples #👉 Check
 Click to open full size. Troubleshooting #If you\u0026rsquo;re experiencing any issues or have a feature request, please check the below resources: WAHA:
 🧩 n8n Integration ⚡ Quick Start 🔧 Install \u0026amp; Update n8n:
 Installation guide Install community nodes Create credentials If that doesn\u0026rsquo;t help - kindly open an issue in the devlikeapro/n8n-nodes-waha GitHub repository, we\u0026rsquo;re happy to help you out!
-`}),e.add({id:36,href:"/docs/integrations/typebot/",title:"Typebot",description:"WAHA Typebot Integration",content:` 👉 Check out WAHA + Typebot templates
+`}),e.add({id:36,href:"/docs/integrations/typebot/",title:"Typebot",description:"WAHA Typebot Integration",content:` 👉 Check out WAHA + Typebot Template
 `}),e.add({id:37,href:"/docs/integrations/python/",title:"Python",description:"WhatsApp API + Python examples.",content:`
 We\u0026rsquo;ve created a few examples on how to interact with WAHA WhatsApp API with Python.
 You can find the examples in examples folder on GitHub with detailed instructions in README.md -\u0026gt;
